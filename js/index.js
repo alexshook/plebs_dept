d2b50f4288d28c32525a760a61328e26ba293215
Alex Shook <alexandra.shook@gmail.com>
2017-06-21 22:09:23 -0400

    Make all the quotes double quotes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-fetch('https://calm-beach-60805.herokuapp.com/').then(function(response) {
+fetch("https://calm-beach-60805.herokuapp.com/").then(function(response) {
   if(response.ok) {
     return response.json();
   }
@@ -8,9 +8,9 @@ fetch('https://calm-beach-60805.herokuapp.com/').then(function(response) {
    var template = Handlebars.compile(source);  
   document.getElementById("products").innerHTML = template({products: products});
 }).then(function() {
-  var elem = document.querySelector('.grid');
+  var elem = document.querySelector(".grid");
   var msnry = new Masonry( elem, {
-    itemSelector: '.grid-item',
+    itemSelector: ".grid-item",
     gutter: 10,
     columnWidth: 200
   });
